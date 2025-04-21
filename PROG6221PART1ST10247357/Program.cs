d0acc61524bca6e382c79497fca1df1a2dc8504f
ST10247357 <st10247357@vcconnect.edu.za>
2025-04-21 14:45:33 +0200

    Used System.Media.SoundPlayer to play 'WelcomeAudio.wav' on application start

--- PROG6221PART1ST10247357/Program.cs
+++ PROG6221PART1ST10247357/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace PROG6221PART1ST10247357
+﻿using System.Media;
+
+namespace PROG6221PART1ST10247357
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!"); 
+            SoundPlayer welcomeMessage = new SoundPlayer(@"WelcomeAudio.wav");// Load the welcome audio file
+            welcomeMessage.Play();// Play the audio
+            Console.ReadKey();
         }
     }
 }
